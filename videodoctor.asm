66aa03b93910a13febe4832d81fb0724e2d567d2
sjse <simon@roust-it.dk>
2015-05-28 21:28:56 +0200

    1st part

--- videodoctor.asm
+++ videodoctor.asm
@@ -1,6 +1,43 @@
         SECTION videodoctor
 
 VideoDoctor
+;*******************************************
+:*TESTCARD BY SIMON JONASSEN
+;*******************************************
+
+	ldx	#$0
+lp1	ldd	#$cfcf
+	std	$400,x
+	std	$402,x
+	ldd	#$9f9f
+	std	$404,x
+	std	$406,x
+	ldd	#$dfdf
+	std	$408,x
+	std	$40a,x
+	ldd	#$8f8f
+	std	$40c,x
+	std	$40e,x
+	ldd	#$efef
+	std	$410,x
+	std	$412,x
+
+	ldd	#$bfbf
+	std	$414,x
+	std	$416,x
+	ldd	#$afaf
+	std	$418,x
+	std	$41a,x
+	ldd	#$ffff
+	std	$41c,x
+	std	$41e,x
+
+	leax	32,x
+
+	cmpx	#32*16
+	bne	lp1
+	rts
+	end	card
         rts
 
         END     SECTION
