2ca5dd5f1d98fb3466f56ddd81aee19b764c5806
Boisy Pitre <boisy@bigmac.local>
2015-06-02 08:56:36 -0500

    More changes

--- menuscreen.asm
+++ menuscreen.asm
@@ -15,14 +15,20 @@ ShowMenuScreen
         lbsr    RAMDoctor
         puls    a,x,y,pc
 IsIt2   cmpa    #'2
-        bne     Exit
+        bne     IsIt3
         lbsr    VideoDoctor
+IsIt3   cmpa    #'3
+        bne     IsIt4
+        lbsr    KeyboardDoctor
+IsIt4   cmpa    #'4
+        bne     Exit
+        lbsr    JoystickDoctor
 Exit    puls    a,x,y,pc
 
 Return  rts
 
 Message 
-        fdb     $0502
+        fdb     $0202
         fcc     "1-"
         fcb     $60
         fcc     "RAM"
@@ -31,10 +37,59 @@ Message
         fcb     $00
         fdb     Return-*-2
 
-        fdb     $0702
+        fdb     $0302
         fcc     "2-"
         fcb     $60
         fcc     "VIDEO"
+        fcb     $60
+        fcc     "DOCTOR"
+        fcb     $00
+        fdb     Return-*-2
+
+        fdb     $0402
+        fcc     "2-"
+        fcb     $60
+        fcc     "SOUND"
+        fcb     $60
+        fcc     "DOCTOR"
+        fcb     $00
+        fdb     Return-*-2
+
+        fdb     $0502
+        fcc     "3-"
+        fcb     $60
+        fcc     "KEYBOARD"
+        fcb     $60
+        fcc     "DOCTOR"
+        fcb     $00
+        fdb     Return-*-2
+
+        fdb     $0602
+        fcc     "4-"
+        fcb     $60
+        fcc     "JOYSTICK"
+        fcb     $60
+        fcc     "DOCTOR"
+        fcb     $00
+        fdb     Return-*-2
+
+        fdb     $0702
+        fcc     "5-"
+        fcb     $60
+        fcc     "SERIAL"
+        fcb     $60
+        fcc     "I"
+        fcb     '/+$40
+        fcc     "O"
+        fcb     $60
+        fcc     "DOCTOR"
+        fcb     $00
+        fdb     Return-*-2
+
+        fdb     $0802
+        fcc     "6-"
+        fcb     $60
+        fcc     "CASSETTE"
         fcb     $60
         fcc     "DOCTOR"
         fcb     $00
--- ramdoctor.asm
+++ ramdoctor.asm
@@ -1,6 +1,22 @@
         SECTION ramdoctor
 
 RAMDoctor
-        rts
+        pshs    d,x,y
+        lda     #$60
+        lbsr    ClearScreen
+        leax    Message,pcr
+        lbsr    PrintBlock
+        lda     #$20
+        lbsr    WaitForKey 
+        puls    d,x,y,pc
+Return  rts
+
+Message
+        fdb     $070D
+        fcc     "PASSED"
+        fcb     $00
+        fdb     Return-*-2
+
+        fdb     $FFFF
 
         END     SECTION
