cda6f6f6b211905b20d5b261096b1f4658a51563
Boisy Pitre <boisy@tee-boy.com>
2015-06-06 06:53:24 -0500

    Incorporated Robert Gault's RAM test into ramdoctor.asm

--- main.asm
+++ main.asm
@@ -25,8 +25,8 @@
 
 EXEC	equ	*
 * Set direct page register
-	lda     #$FF
-	tfr	a,dp
+*	lda     #$FF
+*	tfr	a,dp
 *	setdp	$FF
 
         lds     #$7000         Setup stack
@@ -34,34 +34,19 @@ EXEC	equ	*
 * Disable IRQ and FIRQ
 	orcc	#$50
 
-* Init timing sources
+* Set BASIC Reset Flag
+        clr     $71
 
-* AS DP IS SET TO $FF, then we can do this
-
-	lda	<$03		Disable vsync interrupt generation
+	lda	$FF03		Disable vsync interrupt generation
 	anda	#$FE
-	sta	<$03
+	sta	$FF03
 
-;************************
-; TST is 6+ cycles
-; LDA /LDB is 4
-;************************
 ;	tst	<$02
-	lda	<$02
+	lda	$FF02
 
-	lda	<$01		Disable hsync interrupt generation
+	lda	$FF01		Disable hsync interrupt generation
 	ora	#$01
-	sta	<$01
-
-* Init audio output
-*	lda	PIA1C1		Enable square wave audio output
-*	anda	#$FB
-*	sta	PIA1C1
-*	ldb	#SQWAVE
-*	orb	PIA1D1
-*	stb	PIA1D1
-*	ora	#$04
-*	sta	PIA1C1
+	sta	$FF01
 
         lbsr    ShowTitleScreen
 showagain
