a361d8dd6ed803671626b2757fad44be7e58d88e
RobertGault <robert.gault@att.net>
2015-06-23 23:06:59 -0400

    Some changes and cleaned up code.

--- keyboarddoctor.asm
+++ keyboarddoctor.asm
@@ -9,67 +9,82 @@ KeyboardDoctor lda #$60
 	stx	$88
 	ldx	$FFFE
 	cmpx	#$8C1B
-	beq	c3ky
-	leax	KyCC1a-1,pcr
-	jsr	PRINT
-	lda	#$62
-	sta	[$88]
-	inc	$89
-	leax	KyCC1b-1,pcr
+	lbeq	c3ky
+	leax	KyCC1-1,pcr
 	jsr	PRINT
 a@	jsr	[$A000]
-	beq	a@
+	bne	d@
+	lda	#$7F
+	sta	$FF02
+	lda	$FF00
+	anda	#%01000000
+	bne	a@
+	clra
 d@	cmpa	#3
 	beq	c@
 	sta	$44
-	ldx	#$5AC
+	ldx	#$400+8*32+13		*$5AC
 	stx	$88
 	ldy	#$6060
-	sty	$5AC
-	sty	$5AD
+	sty	$400+8*32+13
+	sty	$400+8*32+14
 	clrb
 	exg	a,b
 	jsr	NUMBR
 	lda	$44
-	leax	MxCC1,pcr
-e@	cmpa	1,x
+	leax	KyCC1b,pcr
+	leay	MxCC1,pcr
+e@	cmpa	,x+
 	beq	f@
-	leax	4,x
-	cmpx	#EndMx1
+	leay	2,y
+	cmpy	#EndMx1
 	bne	e@
 	bra	b@
 f@	bsr	flip
 b@	jsr	[$A000]
-	beq	b@
-	cmpa	$44
+	bne	g@
+	lda	#$7F
+	sta	$FF02
+	lda	$FF00
+	anda	#%01000000
+	bne	b@
+	clra
+g@	cmpa	$44
 	beq	b@
 	bsr	flip
 	bra	d@
 c@	rts
 
-flip	ldb	[2,x]
+flip	ldb	[,y]
 	eorb	#$40
-	stb	[2,x]
-	rts
+	stb	[,y]
+	ldx	,y
+	cmpx	#$461
+	bne	a@
+	ldb	$460+25
+	eorb	#$40
+	stb	$460+25
+a@	rts
 
-c3ky	leax	KyCC3a-1,pcr
-	jsr	PRINT
-	lda	#$62
-	sta	[$88]
-	inc	$89
-	leax	KyCC3b-1,pcr
+c3ky	leax	KyCC3-1,pcr
 	jsr	PRINT
 a@	jsr	[$A000]
-	beq	a@
+	bne	d@
+	lda	#$7F
+	sta	$FF02
+	lda	$FF00
+	anda	#%01000000
+	bne	a@
+	clra
 d@	cmpa	#3
 	beq	c@
 * Add conversion for lower case
 	sta	$44
-	ldx	#$5AC
+	ldx	#$400+8*32+13
 	stx	$88
 	ldy	#$6060
-	sty	$5AC
-	sty	$5AD
+	sty	$400+8*32+13
+	sty	$400+8*32+14
 	clrb
 	exg	a,b
 	jsr	NUMBR
@@ -80,36 +95,36 @@ d@	cmpa	#3
 	cmpb	#$BF
 	bne	g@
 	tfr	b,a
-g@	leax	MxCC3,pcr
-e@	cmpa	1,x
+g@	leax	KyCC3b,pcr
+	leay  MxCC3,pcr
+e@	cmpa	,x+
 	beq	f@
-	leax	4,x
-	cmpx	#EndMx3
+	leay  2,y
+	cmpy	#EndMx3
 	bne	e@
 	bra	b@
 f@	bsr	flip
 b@	jsr	[$A000]
-	beq	b@
-	cmpa	$44
+	bne	h@
+	lda	#$7F
+	sta	$FF02
+	lda	$FF00
+	anda	#%01000000
+	bne	b@
+	clra
+h@	cmpa	$44
 	beq	b@
 * Add conversion for lower case
-	bsr	flip
+	lbsr	flip
 	bra	d@
 c@	rts
-	rts
 
-KyCC3a	fcc	/  ! /
-	fcb	0
-
-KyCC3b	fcc	/ # $ % & ' ( )   * =/
-	fcb	$0D
+KyCC3
 	fcc	/  1 2 3 4 5 6 7 8 9 0 : - BRK/
-	fcb	$0D,$0D
+	fcb	$0D
 	fcc	/AL Q W E R T Y U I O P @ CLR UP/
 	fcb	$0D
-	fcc	/CT A S D F G H J K L ;+ EN <- ->/
-	fcb	$0D,$0D
-	fcc	/                   < > ?/
+	fcc	/CT A S D F G H J K L ; EN <- ->/
 	fcb	$0D
 	fcc	" SHF Z X C V B N M , . / SHF DN"
 	fcb	$0D
@@ -118,19 +133,27 @@ KyCC3b	fcc	/ # $ % & ' ( )   * =/
 	fcc	/       DECI KEY VALUE/
 	fcb	$0D,0
 
+KyCC3b
+	fcb   '1,'2,'3,'4,'5,'6,'7,'8,'9,'0,':,'-,$03
+	fcb	$BF,'Q,'W,'E,'R,'T,'Y,'U,'I,'O,'P','@,$0C,$5E
+	fcb	189,'A,'S,'D,'F,'G,'H,'J,'K,'L,';,$0D,$08,$09
+	fcb	0,'Z,'X,'C,'V,'B,'N,'M,',,'.,'/,0,$0A
+	fcb	$20,103,$04
 
-KyCC1a	fcc	/  ! /
-	fcb	0
+MxCC3	fdb	$400+2,$400+4,$400+6,$400+8,$400+10,$400+12,$400+14,$400+16,$400+18,$400+20,$400+22,$400+24,$400+26
+	fdb	$420,$420+3,$420+5,$420+7,$420+9,$420+11,$420+13,$420+15,$420+17,$420+19,$420+21,$420+23,$420+25,$420+29
+	fdb	$440,$440+3,$440+5,$440+7,$440+9,$440+11,$440+13,$440+15,$440+17,$440+19,$440+21,$440+23,$440+26,$440+30
+	fdb	$460+1,$460+5,$460+7,$460+9,$460+11,$460+13,$460+15,$460+17,$460+19,$460+21,$460+23,$460+25,$460+29
+	fdb	$480+9,$480+23,$480+26
+EndMx3	equ	*
 
-KyCC1b	fcc	/ # $ % & ' ( )   * =/
-	fcb	$0D
+
+KyCC1
 	fcc	/  1 2 3 4 5 6 7 8 9 0 : - BRK/
-	fcb	$0D,$0D
+	fcb	$0D
 	fcc	/UP Q W E R T Y U I O P @ <- ->/
 	fcb	$0D
-	fcc	/DWN A S D F G H J K L ;+ ENT CLR/
-	fcb	$0D,$0D
-	fcc	/                   < > ?/
+	fcc	/DWN A S D F G H J K L ; ENT CLR/
 	fcb	$0D
 	fcc	" SHF Z X C V B N M , . / SHF"
 	fcb	$0D
@@ -138,24 +161,17 @@ KyCC1b	fcc	/ # $ % & ' ( )   * =/
 	fcb	$0D,$0D,$0D
 	fcc	/       DECI KEY VALUE/
 	fcb	$0D,0
-MxCC1	fdb	'!,$402,'",$404,'#,$406,'$,$408,'%,$40A,'&,$40C,'',$40E,'(,$410,'),$412,'*,$416,'=,$418
-	fdb	'1,$422,'2,$424,'3,$426,'4,$428,'5,$42A,'6,$42C,'7,$42E,'8,$430,'9,$432,'0,$434,':,$436,'-,$438
-	fdb	$5E,$460,'Q,$463,'W,$465,'E,$467,'R,$469,'T,$46B,'Y,$46D,'U,$46F,'I,$471,'O,$473,'P,$475,'@,$477
-	fdb	$5F,$460,8,$479,$15,$479,9,$47D,$5D,$47D
-	fdb	$0A,$480,'A,$484,'S,$486,'D,$488,'F,$48A,'G,$48C,'H,$48E,'J,$490,'K,$492,'L,$494,';,$496,'+,$497,$0D,$499,$0C,$49D
-	fdb	$5B,$480,$5C,$49C
-	fdb	'<,$4F3,'>,$4F5,'?,$4F7
-	fdb	'Z,$505,'X,$507,'C,$509,'V,$50B,'B,$50D,'N,$50F,'M,$511,',,$513,'.,$515,'/,$517
-	fdb	$20,$529
-EndMx3	equ	*
 
-MxCC3	fdb	'!,$402,'",$404,'#,$406,'$,$408,'%,$40A,'&,$40C,'',$40E,'(,$410,'),$412,'*,$416,'=,$418
-	fdb	'1,$422,'2,$424,'3,$426,'4,$428,'5,$42A,'6,$42C,'7,$42E,'8,$430,'9,$432,'0,$434,':,$436,'-,$438
-	fdb	$5E,$47D,'Q,$463,'W,$465,'E,$467,'R,$469,'T,$46B,'Y,$46D,'U,$46F,'I,$471,'O,$473,'P,$475,'@,$477
-	fdb	$5F,$47D,8,$49B,$15,$49B,9,$49F,$5D,$49F
-	fdb	$0A,$51D,'A,$483,'S,$485,'D,$487,'F,$489,'G,$48B,'H,$48D,'J,$48F,'K,$491,'L,$493,';,$495,'+,$496,$0D,$498,$0C,$479
-	fdb	$5B,$51D,$5C,$49C
-	fdb	'<,$4F3,'>,$4F5,'?,$4F7
-	fdb	'Z,$505,'X,$507,'C,$509,'V,$50B,'B,$50D,'N,$50F,'M,$511,',,$513,'.,$515,'/,$517
-	fdb	$20,$529,189,$480,$BF,$460,64,$460,103,$537,4,$53A
+KyCC1b
+	fcb   '1,'2,'3,'4,'5,'6,'7,'8,'9,'0,':,'-,$03
+	fcb	$5E,'Q,'W,'E,'R,'T,'Y,'U,'I,'O,'P','@,$08,$09
+	fcb	$0A,'A,'S,'D,'F,'G,'H,'J,'K,'L,';,$0D,$0C
+	fcb	0,'Z,'X,'C,'V,'B,'N,'M,',,'.,'/,0
+	fcb	$20
+
+MxCC1	fdb	$400+2,$400+4,$400+6,$400+8,$400+10,$400+12,$400+14,$400+16,$400+18,$400+20,$400+22,$400+24,$400+26
+	fdb	$420,$420+3,$420+5,$420+7,$420+9,$420+11,$420+13,$420+15,$420+17,$420+19,$420+21,$420+23,$420+25,$420+29
+	fdb	$440,$440+4,$440+6,$440+8,$440+10,$440+12,$440+14,$440+16,$440+18,$440+20,$440+22,$440+24,$440+28
+	fdb	$460+1,$460+5,$460+7,$460+9,$460+11,$460+13,$460+15,$460+17,$460+19,$460+21,$460+23,$460+25
+	fdb	$480+9
 EndMx1	equ	*
